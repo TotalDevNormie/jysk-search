9992b9bd2c7447de54d28e355052b831c57827f6
Vitaly Juhno <vvjuhno@gmail.com>
2025-11-26 11:10:30 +0200

    feat: beta 1.0.0

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -73,6 +73,7 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
 
 function getBaseUrl() {
   if (typeof window !== "undefined") return window.location.origin;
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  if (process.env.RAILWAY_PUBLIC_DOMAIN)
+    return `https://${process.env.RAILWAY_PUBLIC_DOMAIN}`;
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
